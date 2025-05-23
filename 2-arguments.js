3372c89114015ceebe3aa568d584e3221d0c6910
Anas <jonsnow741852@gmail.com>
2025-05-24 00:29:42 +0100

    added a new file

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,4 +1,14 @@
-function arguments () {
+function task (a1, a2, a3) { 
+  
+    console.log(" No Argument found")
     
-    
+   if (task.length <=10){
+console.log("Argument found")
+   }
+   if (task.length<=10) {
+    console.log("Arguments found")
+   }
 }
+task("", "argument", "this is an argument" )
+// task("argument")
+// task("this is an argument")
